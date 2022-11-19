797c17e5bf5f2318b4a82455c68b4e88717881be
Ogara <ya.rom4345@yandex.ru>
2022-11-19 15:02:52 +0200

    add Program

---
+++ Program/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] arrayString = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+
+var list = new List<string>();
+
+for (int i = 0; i < arrayString.Length; i++)
+{
+    if (arrayString[i].Length <= 3)
+    {
+        list.Add(arrayString[i]);
+    }
+}
+
+string[] newArrayString = list.ToArray();
+
+for (int i = 0; i < newArrayString.Length; i++)
+    {
+        Console.Write($"{newArrayString[i]} ");
+    }
